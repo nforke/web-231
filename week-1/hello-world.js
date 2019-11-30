59de2d1c217783a0befa65226c093256be0bd1ca
Nicole Forke <n.forke@gmail.com>
2019-11-30 12:18:54 -0600

    Adding first commit for week-1 hello-world

---
+++ week-1/hello-world.js
@@ -0,0 +1,21 @@
+/*
+=========================
+;Title: Assignment 1.3
+;Author: Professor Krasso
+;Date: 30 November 2019
+;Modified By: Nicole Forke
+;Description: Hello-World Assignment
+=========================
+*/
+
+//start program
+
+/*
+    ;Expected Output:
+    ;Hello World, my name is Nicole Forke
+*/
+
+//output
+console.log("Hello World, my name is Nicole Forke!");
+
+//end program
