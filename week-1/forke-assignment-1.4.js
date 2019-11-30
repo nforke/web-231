829c05a81f7d850d0145259cb0f2c29f9c6e907f
Nicole Forke <n.forke@gmail.com>
2019-11-30 13:19:00 -0600

    Adding first commit for assignment-1.4

---
+++ week-1/forke-assignment-1.4.js
@@ -0,0 +1,35 @@
+/*
+======================================
+;Title: Assignment 1.4
+;Author: Nicole Forke
+;Date: 30 November 2019
+;Modified By: Nicole Forke
+;Description: Primitive Type Variables
+======================================
+*/
+
+//start program
+
+/*
+    ;Expected Output:
+    ;This program should display the contents
+    ;of each variable within the JavaScript code.
+*/
+
+//variable declaration for assignment-1.4:
+
+var firstName = "Nicole";
+var lastName = "Forke";
+var age = "41";
+var street = "1722 N 22nd Street";
+var city = "Beatrice";
+var zipCode = "12345";
+
+//output
+
+console.log(firstName, lastName);
+console.log(age);
+console.log(street);
+console.log(city, zipCode);
+
+//end program
