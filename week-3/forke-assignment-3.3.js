5f39517c795c83ea7de25399cc7637b3a91c64d4
Nicole Forke <n.forke@gmail.com>
2019-12-14 17:55:11 -0600

    Updated

--- week-3/forke-assignment-3.3.js
+++ week-3/forke-assignment-3.3.js
@@ -14,27 +14,27 @@
 */
 
 // variables
-let eventKeyCode = "13";
+let eventKeyCode = 13;
 
 // functions
 switch (eventKeyCode) {
-  case "13":
-  console.log('The enter key was pressed.');
-  break;
+  case 13:
+    console.log('The enter key was pressed.');
+    break;
 
-  case "16":
-  console.log('The shift key was pressed.');
-  break;
+  case 16:
+    console.log('The shift key was pressed.');
+   break;
 
-  case "32":
-  console.log('The spacebar key was pressed.');
-  break;
+  case 32:
+    console.log('The spacebar key was pressed.');
+    break;
 
-  case "8":
-  console.log('The backspace / delete key was pressed.');
-  break;
+  case 8:
+    console.log('The backspace / delete key was pressed.');
+    break;
 
   default:
-  console.log('Unrecognized key.');
-  break;
+    console.log('Unrecognized key.');
+    break;
 }
