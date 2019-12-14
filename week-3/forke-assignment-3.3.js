2e61c51026f47f71da74cd9621caeadc861dda3d
Nicole Forke <n.forke@gmail.com>
2019-12-14 17:44:17 -0600

    Initial commit for Assignment 3.3

---
+++ week-3/forke-assignment-3.3.js
@@ -0,0 +1,40 @@
+/*
+=================================================
+; Title:  Exercise 3.3
+; Author: Nicole Forke
+; Date:   14 December 2019
+; Modified By: Nicole Forke
+; Description: Switch Statements
+;================================================
+*/
+
+/*
+  // Expected output
+  The enter key was pressed
+*/
+
+// variables
+let eventKeyCode = "13";
+
+// functions
+switch (eventKeyCode) {
+  case "13":
+  console.log('The enter key was pressed.');
+  break;
+
+  case "16":
+  console.log('The shift key was pressed.');
+  break;
+
+  case "32":
+  console.log('The spacebar key was pressed.');
+  break;
+
+  case "8":
+  console.log('The backspace / delete key was pressed.');
+  break;
+
+  default:
+  console.log('Unrecognized key.');
+  break;
+}
