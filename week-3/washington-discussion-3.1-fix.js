e81e48b80bbe0976108361a796b3d90cda2a4478
Nicole Forke <n.forke@gmail.com>
2019-12-14 19:26:20 -0600

    Adding Corrected 3.1 Assignments

---
+++ week-3/washington-discussion-3.1-fix.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title:  Discussion-3.1
+; Author: Professor Krasso & w3schools.com
+; Date:   09 December 2019
+; Modified by: Nicole Forke
+; Description: Create a JavaScript program with
+: a while, for, or switch statement with an error.
+;===========================================
+*/
+
+
+// Start program
+var text =""; //removed var i and put into the for loop expression
+
+// for loop counter statement
+for (var i = 0; i < 5; i++) { //added a + sign for the correct increment operator
+  text = "The number is " + i; //removed the + sign next to = sign, removed + and "/n"
+  console.log(text); //moved the console.log into the body of the for loop
+}
+
+// End program
