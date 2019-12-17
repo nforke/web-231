37a2018177378776dd4d6d9581f681ceeedca4af
Nicole Forke <n.forke@gmail.com>
2019-12-16 21:27:20 -0600

    Initial Commit for Week 4

--- week-4/forke-discussion-4.1.js
+++ week-4/forke-discussion-4.1.js
@@ -13,6 +13,12 @@
 // The length of the Array which should be 3.
 */
 
+//statement that displays personal header
+const header = require('./forke-header.js');
+console.log(header.display("Nicole", "Forke", "Assignment 2.4"));
+
+// start program
+
 // variables:
 var a = [10, 20, 50, 5];
 a.length = 3;
@@ -22,3 +28,5 @@ var sum = areduce(function(x, y) { return x + y });
 
 //output showing the array length (3) and the sum of the arrays (80):
 console.log( "There are" + ' ' + alength ' ' + "arrays" + "and" + "here is the sum of the arrays" + ' ' + sum + ".");
+
+// end program
