3bfba22dde01d8a1cb120cc75c72e02acbde4922
Nicole Forke <n.forke@gmail.com>
2019-12-21 10:46:34 -0600

    Initial Commit

---
+++ week-4/forke-assignment-4.2.js
@@ -0,0 +1,41 @@
+/*
+===================================================
+; Title:  Exercise 4.2
+; Author: Nicole Forke
+; Date:   21 December 2019
+; Modified By: Nicole Forke
+; Description: Function using Arrays
+;==================================================
+*/
+
+/*
+  Expected output:
+
+  Apple
+  Orange
+  Banana
+  Mango
+  Pear
+*/
+
+// start program
+
+//statement that displays personal header
+const header = require('./forke-header.js');
+console.log(header.display("Nicole", "Forke", "Assignment 4.2",));
+
+// declared variable using an array for fruits
+var fruits = [ "\nApple", "Orange", "Banana", "Mango", "Pear"];
+
+// getFruit() function using the for loop to call the array
+
+function getFruit (arr) {
+  for (i = 0; i < arr.length; i ++){
+    console.log(arr[i]);
+  }
+}
+
+// Output of getFruit() function
+getFruit(fruits);
+
+// end program
