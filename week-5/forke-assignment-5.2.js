8d5cb5afde5a79c7588b036ee5ba48c95a84b056
Nicole Forke <n.forke@gmail.com>
2020-01-10 15:22:15 -0600

    Initial Commit for Assignment 5.2

---
+++ week-5/forke-assignment-5.2.js
@@ -0,0 +1,38 @@
+/*
+====================================================
+; Title:  Discussion 5.2
+; Author: Nicole Forke
+; Date:   10 January 2020
+; Modified By: Nicole Forke
+; Description: ES5 Built-In Functions
+;===================================================
+*/
+
+/*
+  Expected Output:
+
+  Yogurt
+  Chicken
+  Fish
+  Spaghetti
+  Tiramisu
+*/
+
+//statement that displays personal header
+const header = require('./forke-header.js');
+console.log(header.display("Nicole", "Forke", "Discussion 5.2",));
+
+//start program
+
+//create new line
+console.log("");
+
+//create string array
+var favFood = ["Yogurt", "Chicken", "Fish", "Spaghetti", "Tiramisu"];
+
+//iterating the array for output
+favFood.forEach(function(favFood) {
+  console.log(favFood)
+});
+
+//end program
