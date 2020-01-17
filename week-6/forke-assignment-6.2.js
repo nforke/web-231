b971c039c71e69e991f2a706f8126f4746817635
Nicole Forke <n.forke@gmail.com>
2020-01-16 19:59:23 -0600

    Adding Commit for Assignment 6.2

---
+++ week-6/forke-assignment-6.2.js
@@ -0,0 +1,50 @@
+/*
+=========================================================
+; Title:  Exercise 6.3
+; Author: Nicole Forke
+; Date:   6 January 2020
+; Modified By: Nicole Forke
+; Description: Exception Handling
+;========================================================
+*/
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 6.2
+  Today's Date
+
+  Catch clause: <your custom message goes here>
+  Finally clause reached...
+*/
+
+//statement that displays personal header
+const header = require('./forke-header.js');
+console.log(header.display("Nicole", "Forke", "Exercise 6.2",));
+
+//start program
+
+//create objects and define key/value pairs
+var employee = {
+  userId:"jSmith",
+  password:5020,
+};
+
+//insert space after header
+console.log(" ");
+
+//throw an error
+try {
+  if(employee.userId !== 5021) {
+    throw ("Incorrect Password ");
+  };
+
+} catch (err) {
+  console.log("Error Occurred " + err);
+
+} finally {
+  console.log("reached...")
+};
+
+//end program
